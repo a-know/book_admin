887b3b3977c9eddf9c2b81cd6d95ce89bac1240b
a-know <a.know.dev@gmail.com>
2025-03-21 15:06:50 +0900

    Oops

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,8 +7,6 @@ class Book < ApplicationRecord
 
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
-end
-
 
   belongs_to :publisher
   has_many :book_authors
